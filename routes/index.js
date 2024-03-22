7d8329eacfa4ecb122226500e927ea5005c73f10
Ifeanyi Ezenandu <ezenandu_ice@yahoo.com>
2024-03-22 17:36:00 +0100

    Complete mini message board with added object

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+  res.redirect('/');
+});
+
 module.exports = router;
